3a757b57291fab315c49b49227b7c9628358d9c6
maipham <maiphamt97@gmail.com>
2018-12-20 23:13:38 -0500

    'OFFICIALLY-FINISHED'

--- src/component/Footer/Header.tsx
+++ src/component/Footer/Header.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import './Footer.css';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import Grid from '@material-ui/core/Grid'
-import HeartIcon from '@material-ui/icons/Favorite';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 
 interface HeaderProp {
 }
@@ -22,10 +22,13 @@ export default class Header extends React.Component<HeaderProp, HeaderState> {
                   justify={'flex-end'}>
                 <Grid item={true}
                       style={{display: 'inline-flex'}}>
-                    <p style={{margin: '5px'}}>Made by <a href={'https://github.com/maipham/tic-tac-toe-react/'}
-                                                          style={{textDecoration: 'None'}}>Mai</a>
+                    <p style={{margin: '5px'}}>
+                        <a href={'https://github.com/maipham/tic-tac-toe-react/'}
+                           style={{textDecoration: 'None'}}
+                            target={'_blank'}>
+                        Made by Mai  <FontAwesomeIcon style={{color: '#f50057', fontSize: '25px'}} icon={faGithubAlt}/>
+                    </a>
                     </p>
-                    <HeartIcon style={{color: '#f50057'}}/>
                 </Grid>
             </Grid>
         );
